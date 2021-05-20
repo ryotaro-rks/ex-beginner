0a4e642dd668585707aec03d6eb6e29bd83d26e3
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-20 18:05:47 +0900

    add user to scope. complete task4.

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.domain.User;
 import com.example.form.UserForm;
 
 @Controller
@@ -30,10 +31,18 @@ public class Exam04Controller {
 			return index(model);
 		}
 
-		redirectAttributes.addFlashAttribute("name", form.getName());
-		redirectAttributes.addFlashAttribute("age", form.getAge());
-		redirectAttributes.addFlashAttribute("comment", form.getComment());
+		User user = new User();
+		user.setName(form.getName());
+		user.setAge(form.getAge());
+		user.setComment(form.getComment());
 
+		redirectAttributes.addFlashAttribute("user", user);
+
+		return "redirect:/exam04/toresult";
+	}
+
+	@RequestMapping("toresult")
+	public String toresult() {
 		return "exam04/exam04-result";
 	}
 }
