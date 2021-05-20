8f0076155e1f39ca49ca677aca02708fb14b04b5
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-20 16:55:35 +0900

    演習3完了
    入力画面を作成
    
    結果表示画面作成
    
    applicationスコープを追加。全ての機能完成。

--- src/main/java/com/example/controller/Exam03Controller.java
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -1,5 +1,28 @@
 package com.example.controller;
 
-public class Exam03Controller {
+import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("exam03")
+public class Exam03Controller {
+	@Autowired
+	private ServletContext application;
+
+	@RequestMapping("")
+	public String index() {
+		return "exam03/exam03";
+	}
+
+	@RequestMapping("calc")
+	public String calc(String item1, String item2, String item3) {
+		Integer notaxSum = Integer.parseInt(item1) + Integer.parseInt(item2) + Integer.parseInt(item3);
+		Double taxSum = notaxSum * 1.1;
+		application.setAttribute("notaxSum", notaxSum);
+		application.setAttribute("taxSum", taxSum);
+		return "exam03/exam03-result";
+	}
 }
