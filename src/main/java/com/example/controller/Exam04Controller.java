6178f8292c4e2fd45aa6bcace88b0680fca19e92
ryotaro-rks <ryotaro.seya@rakus.co.jp>
2021-05-20 17:20:54 +0900

    add validation to form

---
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam04")
+public class Exam04Controller {
+	@RequestMapping("")
+	public String index() {
+		return "exam04";
+	}
+}
--- src/main/java/com/example/form/UserForm.java
+++ src/main/java/com/example/form/UserForm.java
@@ -1,8 +1,16 @@
 package com.example.form;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserForm {
+	@NotBlank(message = "名前を入力してください")
 	private String name;
+	@NotNull(message = "年齢を入力してください")
 	private Integer age;
+	@NotBlank(message = "コメントが空です")
+	@Size(min = 1, max = 1000, message = "1~1000文字で入力してください")
 	private String comment;
 
 	@Override
